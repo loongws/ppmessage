2cb87b2843f4fb9a815786ea09de72f59f767be7
Guijin Ding <dingguijin@gmail.com>
2016-04-19 13:29:23 +0800

    message add auth support

--- ppmessage/backend/message.py
+++ ppmessage/backend/message.py
@@ -7,12 +7,40 @@
 # init/message.py 
 # The entry for message service
 #
-#
 
 from ppmessage_mqtt import mqtt_server
+from ppmessage_mqtt import authenticate
+from ppmessage_mqtt import mqtt_authenticate
+
 from tornado.options import parse_command_line
+
+from ppmessage.core.constant import REDIS_HOST
+from ppmessage.core.constant import REDIS_PORT
+
+from ppmessage.db.models import DeviceInfo
+from ppmessage.db.models import ApiTokenData
+
+import redis
+
+class my_authenticate(authenticate):
+    def __init__(self):
+        self.redis = redis.Redis(REDIS_HOST, REDIS_PORT, db=1)
+        return
+    
+    def verify_client_id(self, client_id):
+        _key = DeviceInfo.__tablename__ + ".uuid." + client_id
+        if not self.redis.exists(_key):
+            return False
+        return True
+    
+    def verify_user_password(self, user_id, password):
+        _key = ApiTokenData.__tablename__ + ".api_token." + password
+        if not self.redis.exists(_key):
+            return False
+        return True
 
 if __name__ == "__main__":
     parse_command_line()
+    mqtt_authenticate(my_authenticate)
     mqtt_server()
 
