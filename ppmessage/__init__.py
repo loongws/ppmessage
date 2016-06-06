acbe6746b1bacdd65e751569a21507c961945d9f
Guijin Ding <dingguijin@gmail.com>
2016-06-06 10:17:00 +0800

    start double star

--- ppmessage/__init__.py
+++ ppmessage/__init__.py
@@ -4,10 +4,10 @@ import backend
 version format, MAIN.SUB.HOTFIX.DEV
 
 1.0.0.0:
-Initial to SASS for ppmessage.cn
+Initial to SaaS for ppmessage.cn
 
 2.0.0.0:
-Github to SASS for ppmessage.com
+Github to SaaS for ppmessage.com
 
 2.0.0.1:
 PPCom send<->recv with PPKefu
