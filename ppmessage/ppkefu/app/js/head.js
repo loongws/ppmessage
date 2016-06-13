2896c513eddf291f7f0ef598ca4d9594e9f49343
Guijin Ding <dingguijin@gmail.com>
2016-06-14 06:09:05 +0800

    fix #67 change dist.sh

--- ppmessage/ppkefu/app/js/head.js
+++ ppmessage/ppkefu/app/js/head.js
@@ -1,12 +1,12 @@
 window.ppmessage = {
     "developer_mode": true,
-    "api_key": "NGY3YjM2MGM1ZDExNTRlOGRiNDcxNjhjNjA2Y2ExMDE1YmNiOTVkNA==",
+    "api_key": "NjA0OTZkODNmMmY1ZjU1ZDQwNjE2MjQzMWM3OTZiYWUxNjYyYjg2Zg==",
     "sender_id": "878558045932",
     "server": {
-        "port": "8080",
+        "port": "8945",
         "protocol": "http://",
         "name": "ppmessage.com",
-        "host": "192.168.0.208"
+        "host": "123.57.154.168"
     },
     "version": "1.1.2",
     "disableOnbeforeunload": false
