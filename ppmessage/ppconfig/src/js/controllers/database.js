ffa97b6fbf7a7ae09f352a86822f6a93e87c0a88
Guijin Ding <dingguijin@gmail.com>
2016-06-17 14:24:25 +0800

    add DB type

--- ppmessage/ppconfig/src/js/controllers/database.js
+++ ppmessage/ppconfig/src/js/controllers/database.js
@@ -1,4 +1,16 @@
-function InitializeDatabaseController($scope, $mdDialog) {        
-    console.log("initializedatabsecontroller is called");
+function InitializeDatabaseController($scope, $mdDialog) {
+    $scope.database_types = ["SQLITE", "MYSQL", "PSQL"];
+
+    $scope.hide = function() {
+        $mdDialog.hide();
+    };
+
+    $scope.cancel = function() {
+        $mdDialog.cancel();
+    };
+    
+    $scope.answer = function(answer) {
+        $mdDialog.hide(answer);
+    };
 }
 
