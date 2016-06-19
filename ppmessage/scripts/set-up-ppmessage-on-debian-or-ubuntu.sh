fcc878550d9971582ee78433263387bebebbdddc
Guijin Ding <dingguijin@gmail.com>
2016-06-20 06:17:06 +0800

    fix #73 download geolite with bash

--- ppmessage/scripts/set-up-ppmessage-on-debian-or-ubuntu.sh
+++ ppmessage/scripts/set-up-ppmessage-on-debian-or-ubuntu.sh
@@ -32,18 +32,6 @@ function ppmessage_need_root()
     fi
 }
 
-function download_geolite2() 
-{
-    SCRIPT=$(readlink -f "$0")
-    BASEDIR=$(dirname "${SCRIPT}")
-    APIDIR="${BASEDIR}"/../api/geolite2
-    wget --directory-prefix="${APIDIR}" -c http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz
-    GEOFILE=GeoLite2-City.mmdb.gz
-    GEOPATH="${APIDIR}"/GeoLite2-City.mmdb.gz
-    STEM=$(basename "${GEOFILE}" .gz)
-    gunzip -c "${GEOPATH}" > /"${APIDIR}"/"${STEM}"
-}
-    
 ppmessage_need_root
 
 apt-get update
@@ -135,6 +123,20 @@ pip install \
 pip install hg+https://dingguijin@bitbucket.org/dingguijin/apns-client
 
 
+function download_geolite2() 
+{
+    #SCRIPT=$(readlink -f "$1")
+    BASEDIR=$(dirname "${BASH_SOURCE}")
+    APIDIR="${BASEDIR}"/../api/geolite2
+    echo "${APIDIR}"
+    wget --directory-prefix="${APIDIR}" -c http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz
+    GEOFILE=GeoLite2-City.mmdb.gz
+    GEOPATH="${APIDIR}"/GeoLite2-City.mmdb.gz
+    echo "${GEOPATH}"
+    STEM=$(basename "${GEOFILE}" .gz)
+    gunzip -c "${GEOPATH}" > "${APIDIR}"/"${STEM}"
+}
+    
 download_geolite2
 
 echo "Finish install the requirements of PPMessage, next to run dist.sh with different arguments to start PPMessage."
