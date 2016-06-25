30072a4274ae9f9c9f8a11673f0e57123f51f979
Guijin Ding <dingguijin@gmail.com>
2016-06-25 09:01:52 +0800

    test der2pem

--- ppmessage.py
+++ ppmessage.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python
 
+import sys
 import ppmessage
 
-ppmessage.backend.main._main()
+def _main():
+    if len(sys.argv) == 1:
+        ppmessage.backend.main._main()
+
+    elif len(sys.argv) == 2:
+        if sys.argv[1] == "test_cert":
+            ppmessage.unittest.apns.converter._main()
+
+if __name__ == "__main__":
+    _main()
+
