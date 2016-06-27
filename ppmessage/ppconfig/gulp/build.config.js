9c66a96af9a99a2b76677f88ec1a572125872f92
Guijin Ding <dingguijin@gmail.com>
2016-06-27 17:00:47 +0800

    bower ppconifg ppconsole

--- ppmessage/ppconfig/gulp/build.config.js
+++ ppmessage/ppconfig/gulp/build.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 
     html: ["../src/html/**/*.html", "../src/html/*.html"],
-    icon: ['../../resource/share/bower_components/material-design-icons/*/svg/production/*.svg'],
+    icon: ['../../resource/share/ppconfig/bower_components/material-design-icons/*/svg/production/*.svg'],
     iconPath: "../../resource/assets/ppconfig/static/img/icon/",    
     buildPath: "../../resource/assets/ppconfig/static/dist/",
 
@@ -22,24 +22,24 @@ module.exports = {
     ],
 
     scriptLibFiles: [
-        "../../resource/share/bower_components/js-sha1/build/sha1.min.js",
-        "../../resource/share/bower_components/jquery/dist/jquery.min.js",        
-        "../../resource/share/bower_components/angular/angular.min.js",
+        "../../resource/share/ppconfig/bower_components/js-sha1/build/sha1.min.js",
+        "../../resource/share/ppconfig/bower_components/jquery/dist/jquery.min.js",        
+        "../../resource/share/ppconfig/bower_components/angular/angular.min.js",
         
-        "../../resource/share/bower_components/angular-aria/angular-aria.js",
-        "../../resource/share/bower_components/angular-animate/angular-animate.js",
-        "../../resource/share/bower_components/angular-messages/angular-messages.min.js",
+        "../../resource/share/ppconfig/bower_components/angular-aria/angular-aria.js",
+        "../../resource/share/ppconfig/bower_components/angular-animate/angular-animate.js",
+        "../../resource/share/ppconfig/bower_components/angular-messages/angular-messages.min.js",
         
-        "../../resource/share/bower_components/angular-cookies/angular-cookies.min.js",
-        "../../resource/share/bower_components/angular-file-upload/dist/angular-file-upload.min.js",
-        "../../resource/share/bower_components/angular-block-ui/dist/angular-block-ui.min.js",
-        "../../resource/share/bower_components/angular-utils-pagination/dirPagination.js",
-        "../../resource/share/bower_components/angular-translate/angular-translate.min.js",
-        "../../resource/share/bower_components/angular-ui-router/release/angular-ui-router.min.js",
-        "../../resource/share/bower_components/angular-base64/angular-base64.min.js",
-        "../../resource/share/bower_components/angular-tooltips/dist/angular-tooltips.min.js",
-        "../../resource/share/bower_components/angular-toastr/dist/angular-toastr.tpls.js",
-        "../../resource/share/bower_components/angular-material/angular-material.min.js",
+        "../../resource/share/ppconfig/bower_components/angular-cookies/angular-cookies.min.js",
+        "../../resource/share/ppconfig/bower_components/angular-file-upload/dist/angular-file-upload.min.js",
+        "../../resource/share/ppconfig/bower_components/angular-block-ui/dist/angular-block-ui.min.js",
+        "../../resource/share/ppconfig/bower_components/angular-utils-pagination/dirPagination.js",
+        "../../resource/share/ppconfig/bower_components/angular-translate/angular-translate.min.js",
+        "../../resource/share/ppconfig/bower_components/angular-ui-router/release/angular-ui-router.min.js",
+        "../../resource/share/ppconfig/bower_components/angular-base64/angular-base64.min.js",
+        "../../resource/share/ppconfig/bower_components/angular-tooltips/dist/angular-tooltips.min.js",
+        "../../resource/share/ppconfig/bower_components/angular-toastr/dist/angular-toastr.tpls.js",
+        "../../resource/share/ppconfig/bower_components/angular-material/angular-material.min.js",
         
     ],
     
@@ -49,19 +49,19 @@ module.exports = {
     ],
 
     cssLibFiles: [
-        "../../resource/share/bower_components/material-design-icons/iconfont/material-icons.css",
-        "../../resource/share/bower_components/angular-tooltips/dist/angular-tooltips.min.css",
-        "../../resource/share/bower_components/angular-toastr/dist/angular-toastr.min.css",
-        "../../resource/share/bower_components/angular-material/angular-material.min.css"
+        "../../resource/share/ppconfig/bower_components/material-design-icons/iconfont/material-icons.css",
+        "../../resource/share/ppconfig/bower_components/angular-tooltips/dist/angular-tooltips.min.css",
+        "../../resource/share/ppconfig/bower_components/angular-toastr/dist/angular-toastr.min.css",
+        "../../resource/share/ppconfig/bower_components/angular-material/angular-material.min.css"
     ],
 
     fontFiles: [
-        "../../resource/share/bower_components/material-design-icons/iconfont/codepoints",
-        "../../resource/share/bower_components/material-design-icons/iconfont/MaterialIcons-Regular.eot",
-        "../../resource/share/bower_components/material-design-icons/iconfont/MaterialIcons-Regular.ttf",
-        "../../resource/share/bower_components/material-design-icons/iconfont/MaterialIcons-Regular.ijmap",
-        "../../resource/share/bower_components/material-design-icons/iconfont/MaterialIcons-Regular.woff",
-        "../../resource/share/bower_components/material-design-icons/iconfont/MaterialIcons-Regular.svg",
-        "../../resource/share/bower_components/material-design-icons/iconfont/MaterialIcons-Regular.woff2"
+        "../../resource/share/ppconfig/bower_components/material-design-icons/iconfont/codepoints",
+        "../../resource/share/ppconfig/bower_components/material-design-icons/iconfont/MaterialIcons-Regular.eot",
+        "../../resource/share/ppconfig/bower_components/material-design-icons/iconfont/MaterialIcons-Regular.ttf",
+        "../../resource/share/ppconfig/bower_components/material-design-icons/iconfont/MaterialIcons-Regular.ijmap",
+        "../../resource/share/ppconfig/bower_components/material-design-icons/iconfont/MaterialIcons-Regular.woff",
+        "../../resource/share/ppconfig/bower_components/material-design-icons/iconfont/MaterialIcons-Regular.svg",
+        "../../resource/share/ppconfig/bower_components/material-design-icons/iconfont/MaterialIcons-Regular.woff2"
     ],
 };
